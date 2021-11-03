a0f93e07bbf233736e4317bdb2e68be3af625f18
JassonLuiz <jassonluizjr@hotmail.com>
2021-11-02 23:45:41 -0300

    first commit

---
+++ src/main/java/br/github/jassonluiz/importbr/ImportbrApplication.java
@@ -0,0 +1,13 @@
+package br.github.jassonluiz.importbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/github/jassonluiz/importbr/ImportbrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.github.jassonluiz.importbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
